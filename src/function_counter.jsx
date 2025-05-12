2dd9a587814b9a4b76224f7dbfa7612ce4ef0d33
m7medayman <m7medmahgoop@gmail.com>
2025-05-12 18:05:28 +0300

    ..

--- src/function_counter.jsx
+++ src/function_counter.jsx
@@ -3,20 +3,16 @@ import "./Counter.css";
 
 
 const CounterFunc=()=>{
-  // Use useState to manage the 'count' state variable
   const [count, setCount] = useState(0);
 
-  // Function to increment the count
   const increment = () => {
     setCount(count + 1);
   };
 
-  // Function to decrement the count
   const decrement = () => {
     setCount(count - 1);
   };
 
-  // Render the counter UI
   return (
     <div className="counter-container">
       <h1 className="counter-display">Counter: {count}</h1>
